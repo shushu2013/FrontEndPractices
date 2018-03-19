05f20c45a2ca8d3ceefa15e6949096b6f32edf35
wwb <1252093436@qq.com>
2018-03-19 14:51:09 +0800

    revise singlePage

--- singlePage/gulpfile.js
+++ singlePage/gulpfile.js
@@ -4,6 +4,7 @@ var less = require('gulp-less');
 var ejs = require('gulp-ejs');
 var data = require('gulp-data');
 var cleanCSS = require('gulp-clean-css');
+var clean = require('gulp-clean');
 var webserver = require('gulp-webserver');
 var plumber = require('gulp-plumber');
 
@@ -15,7 +16,7 @@ var srcPath = {
     allless: ['src/less/**/*.less', 'src/less/*.less'],
     ejs: 'src/*.ejs',
     json: 'src/temple/*.json',
-    otherSource: ['src/css/*.css', 'src/fonts/*', 'src/images/*', 'src/js/*.js']
+    staticSource: ['src/css/*.css', 'src/fonts/*', 'src/images/*', 'src/js/*.js']
 };
 
 var buildPath = {
@@ -24,14 +25,17 @@ var buildPath = {
     html: 'dist',
 };
 
+// 标志是否 clean
+var IsClean = false;
+
 
 // 默认执行的任务
-gulp.task('default', ['other', 'less', 'ejs', 'watch', 'webserver'], function() {
+gulp.task('default', ['webserver'], function() {
     console.log('default task ok!');
 })
 
 // 本地服务器 + 热更新
-gulp.task('webserver', function() {
+gulp.task('webserver', ['copy', 'less', 'ejs', 'watch'], function() {
     gulp.src('dist')    // 监控的目录
         .pipe(webserver({
             port: 5000,         // 设置端口
@@ -40,9 +44,8 @@ gulp.task('webserver', function() {
         }))
 })
 
-
 // less 处理
-gulp.task('less', function() {
+gulp.task('less', ['clean'], function() {
     return gulp.src(srcPath.less)
         .pipe(plumber())
         .pipe(less())
@@ -63,7 +66,7 @@ var getJsonData = function(file) {
     return data;
 }
 // ejs 处理
-gulp.task('ejs', function() {
+gulp.task('ejs', ['clean'], function() {
     return gulp.src(srcPath.ejs)
         .pipe(plumber())
         .pipe(data(getJsonData))
@@ -71,14 +74,25 @@ gulp.task('ejs', function() {
         .pipe(gulp.dest(buildPath.html))
 });
 
-// other 其他任务处理
-gulp.task('other', function() {
-    gulp.src(srcPath.otherSource)
+// copy 任务处理
+gulp.task('copy', ['clean'], function() {
+    gulp.src(srcPath.staticSource)
         .pipe(gulp.dest(function(file) {
             var toPath = buildPath.root + '/' + path.basename(file.base);
             // console.log(toPath)
             return toPath;
         }))
+})
+
+// clean 任务处理
+gulp.task('clean', function() {
+    if (!IsClean) {
+        IsClean = true;
+        return gulp.src(buildPath.root)
+            .pipe(plumber())
+            .pipe(clean({force: true}))
+    }
+    return;
 })
 
 // 监听任务
@@ -88,5 +102,5 @@ gulp.task('watch', function() {
     gulp.watch(srcPath.allless, ['less']); // 监听所有的 less 文件
     gulp.watch(srcPath.ejs, ['ejs']);     // 监听 ejs 文件
     gulp.watch(srcPath.json, ['ejs']);    // 监听 json 文件
-    gulp.watch(srcPath.otherSource, ['other'])
+    gulp.watch(srcPath.staticSource, ['copy'])
 })
